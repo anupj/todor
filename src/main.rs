914a806afdb31e19ca8e71bc51bc2f1cb294e980
Anup Jadhav <anup.jadhav@gmail.com>
2023-01-17 12:11:53 +0000

    Initial implementation with SurrealDB complete

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,97 @@
 #![allow(dead_code)]
-#![allow(unused_imports)]
+#![allow(unused)]
 
 mod todo;
 
-use rand::{self, Rng};
-use std::{collections::BTreeMap, error::Error, fmt, sync::Arc, thread};
-use surrealdb::{
-    sql::{self, Data},
-    sql::{Thing, Value},
-    Datastore, Session,
-};
-use tokio;
-use tokio::sync::Mutex;
+use anyhow::{anyhow, Ok, Result};
+use std::collections::BTreeMap;
+use surrealdb::sql::{thing, Datetime, Object, Thing, Value};
+use surrealdb::{Datastore, Response, Session};
+
+type DB = (Datastore, Session);
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    // let ds = Datastore::new("memory").await?;
-    let ds = Datastore::new("file://test.db").await?;
+async fn main() -> Result<()> {
+    // Start by creating a datastore and session with
+    // namespace and db name
+    let db: &DB = &(
+        Datastore::new("memory").await?,
+        Session::for_db("my_ns", "my_db"),
+    );
+    let (ds, ses) = db;
 
-    // `for_kv` - create a session with root authentication
-    // `with_ns` - set the selected namespace for the sesh
-    // `with_db` - set the db for the sesh
-    let ses = Session::for_kv().with_ns("test").with_db("test");
+    // --- Create
+    let t1 = create_task(db, "Task 01", 10).await?;
+    let t2 = create_task(db, "Task 02", 7).await?;
 
-    let entry_num = 2;
-    for i in 0..entry_num {
-        let mut vars = BTreeMap::new();
-        vars.insert(
-            "num".to_string(),
-            sql::Value::Number(sql::Number::Int(i as i64)),
-        );
-        // The documentation for `execute()` is not that
-        // helpful
-        // The query will create a table *entry* and
-        // will set the column `when` to current time
-        // and the column `num` to $num
-        ds.execute(
-            r#"CREATE entry SET when = time::now(), num = $num;"#,
-            &ses,
-            Some(vars),
-            false,
-        )
-        .await?;
+    // --- Merge
+    let sql = "UPDATE $th MERGE $data RETURN id";
+    let data_map: BTreeMap<String, Value> = [
+        ("title".into(), "Task 02 UPDATED".into()),
+        ("done".into(), true.into()),
+    ]
+    .into();
+    let vars: BTreeMap<String, Value> = [
+        // `thing` will parse t2
+        // and ensure its well formatted
+        ("th".into(), thing(&t2)?.into()),
+        ("data".into(), data_map.into()),
+    ]
+    .into();
+
+    // we set `strict` to true
+    // because we want to ensure
+    // fields exist before updating
+    ds.execute(sql, ses, Some(vars), true).await?;
+
+    // --- Delete
+    let sql = "DELETE $th";
+    let vars: BTreeMap<String, Value> = [("th".into(), thing(&t1)?.into())].into();
+    ds.execute(sql, ses, Some(vars), true).await?;
+
+    // --- Select
+    let sql = "SELECT * from task";
+    let ress = ds.execute(sql, ses, None, false).await?;
+    for object in into_iter_objects(ress)? {
+        println!("record {}", object?);
     }
-
-    let select_response = &ds
-        .execute("SELECT * FROM entry;", &ses, None, false)
-        .await?;
-
-    let select_result = select_response[0].output().unwrap();
-
-    let mut id_acc = vec![];
-
-    if let Value::Array(rows) = select_result {
-        for row in rows.iter() {
-            if let Value::Object(obj) = row {
-                println!("{:?}", obj);
-                id_acc.push(obj.rid().unwrap());
-            }
-        }
-    }
-
-    println!("time taken:{}", select_response[0].speed());
 
     Ok(())
+}
+
+async fn create_task((ds, ses): &DB, title: &str, priority: i32) -> Result<String> {
+    let sql = "CREATE task CONTENT $data";
+
+    let data_map: BTreeMap<String, Value> = [
+        ("title".into(), title.into()),
+        ("priority".into(), priority.into()),
+    ]
+    .into();
+
+    let vars: BTreeMap<String, Value> = [("data".into(), data_map.into())].into();
+
+    // setting `strict` param to `false`
+    // because we want to create table/column
+    // that might not be pre-defined in the db
+    let ress = ds.execute(sql, ses, Some(vars), false).await?;
+    into_iter_objects(ress)?
+        .next()
+        .transpose()?
+        .and_then(|obj| obj.get("id").map(|id| id.to_string()))
+        .ok_or_else(|| anyhow!("No id returned"))
+}
+
+fn into_iter_objects(ress: Vec<Response>) -> Result<impl Iterator<Item = Result<Object>>> {
+    let res = ress.into_iter().next().map(|rp| rp.result).transpose()?;
+
+    match res {
+        Some(Value::Array(arr)) => {
+            let it = arr.into_iter().map(|v| match v {
+                Value::Object(object) => Ok(object),
+                _ => Err(anyhow!("A record was not an Object")),
+            });
+            Ok(it)
+        }
+        _ => Err(anyhow!("No records founds")),
+    }
 }
